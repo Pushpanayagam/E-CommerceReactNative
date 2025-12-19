e12f4251755bc2410e5e00504589f449b79cfa90
Pushpanayagam <48618035+Pushpanayagam@users.noreply.github.com>
2025-12-19 08:17:48 +0530

    YourIP

--- frontEnd1/api.js
+++ frontEnd1/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 const client = axios.create({
-    baseURL:"http://192.168.8.135:3000/api" // replace with your backend URLcd
+    baseURL:"http://yourIP:3000/api" // replace with your backend URLcd
 });
 
 client.interceptors.request.use(async config => {
